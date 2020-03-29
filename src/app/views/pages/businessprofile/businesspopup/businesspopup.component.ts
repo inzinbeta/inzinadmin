763e3ddd6a7b31572b655ff034901c6092d19b04
Shubham Dixit <shubhamdixit@Shubhams-MacBook-Air.local>
2020-03-29 13:31:42 +0530

    updated

--- src/app/views/pages/businessprofile/businesspopup/businesspopup.component.ts
+++ src/app/views/pages/businessprofile/businesspopup/businesspopup.component.ts
@@ -36,10 +36,19 @@ export class BusinesspopupComponent implements OnInit {
   fileData2: File = null;
   fileData3: File = null;
   fileData4: File = null;
+  re1:File=null;
+  re2:File=null;
+  re3:File=null;
+
   previewUrl1: any = "assets/images/download.jpeg";
   previewUrl2: any = "assets/images/download.jpeg";
   previewUrl3: any = "assets/images/download.jpeg";
   previewUrl4: any = "assets/images/download.jpeg";
+
+
+  rep1: any = "assets/images/download.jpeg";
+  rep2: any = "assets/images/download.jpeg";
+  rep3: any = "assets/images/download.jpeg";
 
   fileUploadProgress: string = null;
   uploadedFilePath: string = null;
@@ -55,30 +64,28 @@ export class BusinesspopupComponent implements OnInit {
 
 
 
-  fileProgress(fileInput: any, num) {
+  fileProgressa(fileInput: any, num) {
   
     this.fileData = <File>fileInput.target.files[0];
     switch (num) {
       case 1:
-        this.fileData1 = <File>fileInput.target.files[0];
+        this.re1 = <File>fileInput.target.files[0];
         break;
       case 2:
-        this.fileData2 = <File>fileInput.target.files[0];
+        this.re2 = <File>fileInput.target.files[0];
         break;
       case 3:
-        this.fileData3 = <File>fileInput.target.files[0];
+        this.re3 = <File>fileInput.target.files[0];
         break;
-      case 4:
-        this.fileData4 = <File>fileInput.target.files[0];
-        break;
+    
       default:
       // code block
     }
 
-    this.preview(this.fileData, num);
+    this.previewa(this.fileData, num);
   }
 
-  preview(filedata, num) {
+  previewa(filedata, num) {
     // Show preview 
     var mimeType = filedata.type;
     if (mimeType.match(/image\/*/) == null) {
@@ -90,23 +97,82 @@ export class BusinesspopupComponent implements OnInit {
     reader.onload = (_event) => {
       switch (num) {
         case 1:
-          this.previewUrl1 = reader.result;
+          this.rep1 = reader.result;
           break;
         case 2:
-          this.previewUrl2 = reader.result;
+          this.rep2 = reader.result;
           break;
         case 3:
-          this.previewUrl3 = reader.result;
+          this.rep3 = reader.result;
           break;
-        case 4:
-          this.previewUrl4 = reader.result;
-          break;
+       
         default:
         // code block
       }
 
     }
   }
+
+/**
+ * Related images
+ */
+
+
+fileProgress(fileInput: any, num) {
+  
+  this.fileData = <File>fileInput.target.files[0];
+  switch (num) {
+    case 1:
+      this.fileData1 = <File>fileInput.target.files[0];
+      break;
+    case 2:
+      this.fileData2 = <File>fileInput.target.files[0];
+      break;
+    case 3:
+      this.fileData3 = <File>fileInput.target.files[0];
+      break;
+    case 4:
+      this.fileData4 = <File>fileInput.target.files[0];
+      break;
+    default:
+    // code block
+  }
+
+  this.preview(this.fileData, num);
+}
+
+preview(filedata, num) {
+  // Show preview 
+  var mimeType = filedata.type;
+  if (mimeType.match(/image\/*/) == null) {
+    return;
+  }
+
+  var reader = new FileReader();
+  reader.readAsDataURL(this.fileData);
+  reader.onload = (_event) => {
+    switch (num) {
+      case 1:
+        this.previewUrl1 = reader.result;
+        break;
+      case 2:
+        this.previewUrl2 = reader.result;
+        break;
+      case 3:
+        this.previewUrl3 = reader.result;
+        break;
+      case 4:
+        this.previewUrl4 = reader.result;
+        break;
+      default:
+      // code block
+    }
+
+  }
+}
+
+
+
   ngOnInit() {
     console.log("page");
     this.buildItemForm(this.data.payload)
@@ -189,6 +255,9 @@ export class BusinesspopupComponent implements OnInit {
       this.previewUrl2 = this.linkImg(item.slider2);
       this.previewUrl3 = this.linkImg(item.slider3);
       this.previewUrl4 = this.linkImg(item.slider4);
+      this.rep1 = this.linkImg(item.related1);
+      this.rep2 = this.linkImg(item.related2);
+      this.rep3 = this.linkImg(item.related3);
       //this.fileDatalogo=this.linkImg(item.imagelogo);
       //this.fileDatasidebar=this.linkImg(item.imagesidebar);
     }
@@ -234,24 +303,24 @@ export class BusinesspopupComponent implements OnInit {
         break;
       case 10:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
       case 50:
         file_extt2 = this.fileData2.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt1[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
       case 60:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt2 = this.fileData1.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt2[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
+        file_extt2 = this.fileData2.name.split(".");
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt2[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
       case 100:
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
 
@@ -259,64 +328,64 @@ export class BusinesspopupComponent implements OnInit {
 
       case 110:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
       case 150:
         file_extt2 = this.fileData2.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt1[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
 
         break;
       case 160:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
         file_extt2 = this.fileData2.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt1[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
 
       case 200:
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
 
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
       case 210:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
 
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
 
       case 250:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
 
 
-        file_extt3 = this.fileData1.name.split(".");
+        file_extt3 = this.fileData3.name.split(".");
         fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
 
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
 
         break;
         case 260:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
         file_extt2 = this.fileData1.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt2[1]}`);
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt2[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
 
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
 
@@ -324,25 +393,25 @@ export class BusinesspopupComponent implements OnInit {
         case 300:
         
   
-          file_extt3 = this.fileData1.name.split(".");
-          fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+          file_extt3 = this.fileData3.name.split(".");
+          fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
   
-          file_extt4 = this.fileData1.name.split(".");
-          fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+          file_extt4 = this.fileData4.name.split(".");
+          fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
   
           fd.append('formavalues', JSON.stringify(this.itemForm.value));
 
 
           case 310:
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
         
 
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
 
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
 
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
 
@@ -350,28 +419,28 @@ export class BusinesspopupComponent implements OnInit {
         case 350:
           
           file_extt2 = this.fileData1.name.split(".");
-          fd.append(`slider2`, this.fileData, `categoryicon.${file_extt2[1]}`);
+          fd.append(`slider2`, this.fileData1, `categoryicon.${file_extt2[1]}`);
   
-          file_extt3 = this.fileData1.name.split(".");
-          fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+          file_extt3 = this.fileData3.name.split(".");
+          fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
   
-          file_extt4 = this.fileData1.name.split(".");
-          fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+          file_extt4 = this.fileData4.name.split(".");
+          fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
   
           fd.append('formavalues', JSON.stringify(this.itemForm.value));
           break;
       case 360:
        
         file_extt1 = this.fileData1.name.split(".");
-        fd.append(`slider1`, this.fileData, `categoryicon.${file_extt1[1]}`);
-        file_extt2 = this.fileData1.name.split(".");
-        fd.append(`slider2`, this.fileData, `categoryicon.${file_extt2[1]}`);
+        fd.append(`slider1`, this.fileData1, `categoryicon.${file_extt1[1]}`);
+        file_extt2 = this.fileData2.name.split(".");
+        fd.append(`slider2`, this.fileData2, `categoryicon.${file_extt2[1]}`);
 
-        file_extt3 = this.fileData1.name.split(".");
-        fd.append(`slider3`, this.fileData, `categoryicon.${file_extt3[1]}`);
+        file_extt3 = this.fileData3.name.split(".");
+        fd.append(`slider3`, this.fileData3, `categoryicon.${file_extt3[1]}`);
 
-        file_extt4 = this.fileData1.name.split(".");
-        fd.append(`slider4`, this.fileData, `categoryicon.${file_extt4[1]}`);
+        file_extt4 = this.fileData4.name.split(".");
+        fd.append(`slider4`, this.fileData4, `categoryicon.${file_extt4[1]}`);
 
         fd.append('formavalues', JSON.stringify(this.itemForm.value));
         break;
@@ -380,6 +449,89 @@ export class BusinesspopupComponent implements OnInit {
 
 
 
+
+    if(this.re1 && this.re2 && this.re3)
+    {
+      file_extt1 = this.re1.name.split(".");
+        fd.append(`related1`, this.re1, `categoryicon.${file_extt1[1]}`);
+        file_extt2 = this.re2.name.split(".");
+        fd.append(`related2`, this.re2, `categoryicon.${file_extt2[1]}`);
+
+        file_extt3 = this.re3.name.split(".");
+        fd.append(`related3`, this.re3, `categoryicon.${file_extt3[1]}`);
+
+       
+
+    }
+
+
+    else if(this.re1 && !this.re2 && !this.re3)
+    {
+      file_extt1 = this.re1.name.split(".");
+        fd.append(`related1`, this.re1, `categoryicon.${file_extt1[1]}`);
+      
+       
+
+    }
+
+    else if(this.re1 && this.re2 && ! this.re3)
+    {
+      file_extt1 = this.re1.name.split(".");
+        fd.append(`related1`, this.re1, `categoryicon.${file_extt1[1]}`);
+        file_extt2 = this.re2.name.split(".");
+        fd.append(`related2`, this.re2, `categoryicon.${file_extt2[1]}`);
+
+        
+       
+
+    }
+
+    else if(this.re1 && ! this.re2 && this.re3)
+    {
+      file_extt1 = this.re1.name.split(".");
+        fd.append(`related1`, this.re1, `categoryicon.${file_extt1[1]}`);
+        file_extt2 = this.re2.name.split(".");
+        fd.append(`related2`, this.re2, `categoryicon.${file_extt2[1]}`);
+
+
+       
+
+    }
+
+    else if(! this.re1 && this.re2 && this.re3)
+    {
+    
+        file_extt2 = this.re2.name.split(".");
+        fd.append(`related2`, this.re2, `categoryicon.${file_extt2[1]}`);
+
+        file_extt3 = this.re3.name.split(".");
+        fd.append(`related3`, this.re3, `categoryicon.${file_extt3[1]}`);
+
+       
+
+    }
+
+   else  if(! this.re1 && this.re2 && ! this.re3)
+    {
+    
+        file_extt2 = this.re2.name.split(".");
+        fd.append(`related2`, this.re2, `categoryicon.${file_extt2[1]}`);
+
+       
+
+       
+
+    }
+   else  if(!this.re1 && ! this.re2 && this.re3)
+    {
+    
+
+        file_extt3 = this.re3.name.split(".");
+        fd.append(`related3`, this.re3, `categoryicon.${file_extt3[1]}`);
+
+       
+
+    }
 
 
 
